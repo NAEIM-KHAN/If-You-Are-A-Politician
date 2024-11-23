e37c90ae757ec5ebaaa64b2d7c2389b2b8d5db19
NAEIM KHAN <154927340+NAEIM-KHAN@users.noreply.github.com>
2024-11-23 18:46:39 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="If You Are A Politician! - Spinning Wheel Game by NAEIM KHAN">
+  <meta name="keywords" content="If You Are A Politician!, Politician Game, NAEIM KHAN's Games">
+  <meta name="author" content="NAEIM KHAN">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>If You Are A Politician! - Spinning Wheel Game</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <h1 class="top">If You Are A Politician!</h1>
+  <div class="container">
+    <div class="wheel-container">
+      <canvas id="wheel"></canvas>
+      <button id="spin" class="btn">Spin</button>
+    </div>
+    <div id="popup">
+      <button id="closePopup">✖</button> <!-- Close button -->
+      <img id="resultImage" src="" alt="Result">
+    </div>    
+  </div>
+  <a href="https://naeim.live" target="_blank">Developer👈</a>
+  <script src="script.js"></script>
+</body>
+</html>
